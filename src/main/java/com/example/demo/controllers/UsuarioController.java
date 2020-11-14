7eebc52352e308b3a746d98fafef555646e3da3d
Pablo Tedesco <tedesco8@hotmail.com>
2020-11-14 19:19:31 -0300

    creando controller, servicio y repositorio

---
+++ src/main/java/com/example/demo/controllers/UsuarioController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controllers;
+import com.example.demo.models.UsuarioModel;
+import com.example.demo.services.UsuarioService;
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+    @Autowired
+    UsuarioService usuarioService;
+
+    @GetMapping()
+    public ArrayList<UsuarioModel> obtenerUsuario() {
+        return usuarioService.obtenerUsuario();
+    }
+
+    @PostMapping()
+    public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usuario) {
+        return this.usuarioService.guardarUsuario(usuario);
+    }
+
+}
---
+++ src/main/java/com/example/demo/services/UsuarioService.java
@@ -0,0 +1,21 @@
+package com.example.demo.services;
+import com.example.demo.models.UsuarioModel;
+import com.example.demo.repositories.UsuarioRepository;
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    public ArrayList<UsuarioModel> obtenerUsuario() {
+        return (ArrayList<UsuarioModel>) usuarioRepository.findAll();
+    }
+
+    public UsuarioModel guardarUsuario(UsuarioModel usuario) {
+        return usuarioRepository.save(usuario);
+    }
+    
+}
